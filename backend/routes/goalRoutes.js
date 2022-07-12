43b30105cd81055a27077d90060be0b62279b0a7
Mary Prince <mleeherr@gmail.com>
2022-07-11 20:31:17 -0400

    updating readme

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,26 +1,30 @@
-const express = require('express')
+const express = require("express");
 //we are using the router method availble in the express framework here
-const router = express.Router()
+const router = express.Router();
 
-const {getGoals, updateGoal, setGoal, deleteGoal} = require('../controllers/goalController')
+const {
+  getGoals,
+  updateGoal,
+  setGoal,
+  deleteGoal,
+} = require("../controllers/goalController");
 
-//alrighty, so if someone goes to the route that comes down here, it is going to run a get request here ('/'), to respond with - if 200 then return a json object with a 'message' key and a 'get goals' string as the value.  EDIT: We took these routes and are using them via the controller. 
+//alrighty, so if someone goes to the route that comes down here, it is going to run a get request here ('/'), to respond with - if 200 then return a json object with a 'message' key and a 'get goals' string as the value.  EDIT: We took these routes and are using them via the controller.
 // router.get('/', (req, res) =>{
 //     res.status(200).json({message :'Set Goal'})
-//     }) 
+//     })
 //now becomes the below
-router.get('/', getGoals) 
+router.get("/", getGoals);
 
 //to create,  we need to post
-router.post('/', setGoal)
+router.post("/", setGoal);
 
-  //for the put request, we need the id - because we have to point to which goal we want to update.
-  //note the back ticks - so the /:id is the PARAM, and in the object/message we are using the variable ${req.params.id} to point to that variable- my guess is that this will just show us the id number in a string?
-router.put('/:id', updateGoal)
+//for the put request, we need the id - because we have to point to which goal we want to update.
+//note the back ticks - so the /:id is the PARAM, and in the object/message we are using the variable ${req.params.id} to point to that variable- my guess is that this will just show us the id number in a string?
+router.put("/:id", updateGoal);
 
 //we are going to use the id as well for delete
-router.delete('/:id', deleteGoal)
+router.delete("/:id", deleteGoal);
 
 // ok so this is our module exporting the router variable we created - per the google "Module exports are the instruction that tells Node. js which bits of code (functions, objects, strings, etc.) to “export” from a given file so other files are allowed to access the exported code"
-module.exports = router
-
+module.exports = router;
--- readme.md
+++ readme.md
@@ -9,6 +9,8 @@ Now would be the time to decide.  I think we can do this! So, the example that T
 [19:47](https://youtu.be/-0exw-9YJBo?t=1187) - **we did it bois!  we made and tested routes**
  so our routes work, but they are ONLY routes at this point- and those routes only return 'messages' as defined in the object attached to the request.
 
+ [25:25](https://youtu.be/-0exw-9YJBo?t=1525) - note that HERE Traversy decides to put the routes in getRoutes into consolidated lines of code (so, because get and post both use '/' and '/:id', respectively, you COULD do something like: `router.route('/').get(getGoals).post(setGoal)` - BUT I'm, choosing not to do that just yet)
+
 
 ## Technologies Used
 - Node
